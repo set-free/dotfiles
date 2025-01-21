354c1cc60cd380176a0b1b54eb936fdfad577810
Laptop <s.i.v.892@gmail.com>
2025-01-21 16:42:50 +0300

    fix mypy in null-ls

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -4,7 +4,12 @@ return {
     local null_ls = require 'null-ls'
     null_ls.setup {
       sources = {
-        null_ls.builtins.diagnostics.mypy,
+        null_ls.builtins.diagnostics.mypy.with({
+           extra_args = function()
+           local virtual = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX") or "/usr"
+           return { "--python-executable", virtual .. "/bin/python3" }
+           end,
+        }),
         null_ls.builtins.completion.nvim_snippets,
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.completion.spell,
